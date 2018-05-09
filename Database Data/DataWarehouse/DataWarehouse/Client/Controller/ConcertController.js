1d07fb2f6bdd57734dc012ba60224366665c747b
unknown <nikb248@gmail.com>
2018-05-08 20:42:00 -0400

    May 8, 2018 - initial commit

---
+++ Database Data/DataWarehouse/DataWarehouse/Client/Controller/ConcertController.js
@@ -0,0 +1,7 @@
+﻿
+angular.module('dataWarehouse')
+    .controller('ConcertController', ['$scope', '$state',
+    function ($scope, $state) {
+        $scope.vm = this;
+        vm.message = "Hello there, everyone!";
+    }]);
---
+++ Database Data/DataWarehouse/DataWarehouse/Components/userInformation.js
@@ -0,0 +1,35 @@
+﻿//(function () {
+//    'use strict';
+
+//    angular
+//        .module('app')
+//        .controller('User', user);
+
+//    function user()
+//    {
+//        var vm = this;
+//        vm.food = 'pizza';
+//        vm.people = new Array("nik", "stephen");
+//    }
+
+//})();
+'use strict'
+
+angular.module('dataWarehouse.lxa')
+    .controller('UserController', ['$scope', function ($scope) {
+        //$scope.user = { firstName: 'Nik', lastName: 'Bournelis', ekNumber: '1473' };
+        //$scope.firstName = "test";
+        //$scope.lastName = 'test2';
+    }])
+    // change @ to = for two-way binding (once c# variables are created)
+    .directive('myUser', function () {
+        return {
+            restrict: "A",
+            scope: {
+                firstName: '@?firstName',
+                lastName: '@?lastName',
+                ekNumber: '@?ekNumber'
+            },
+            templateUrl: 'Components/userInformation.html'
+        }
+    })
