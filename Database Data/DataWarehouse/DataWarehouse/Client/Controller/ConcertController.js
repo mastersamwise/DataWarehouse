e1b3a9fb4cb17f4be899d06a7bdf61db193af976
mastersamwise <nikb248@gmail.com>
2018-05-11 22:30:54 -0400

    - delete old DataWarehouse project to start fresh

--- Database Data/DataWarehouse/DataWarehouse/Client/Controller/ConcertController.js
+++
@@ -1,7 +0,0 @@
-﻿
-angular.module('dataWarehouse')
-    .controller('ConcertController', ['$scope', '$state',
-    function ($scope, $state) {
-        $scope.vm = this;
-        vm.message = "Hello there, everyone!";
-    }]);
