55c12beba51545f17e8f44766a2a35d7bd1347ab
mastersamwise <nikb248@gmail.com>
2018-06-17 15:39:44 -0400

    added concert component

--- DataWarehouse/src/app/app.module.ts
+++ DataWarehouse/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ConcertComponent } from './concert/concert.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConcertComponent
   ],
   imports: [
     BrowserModule
