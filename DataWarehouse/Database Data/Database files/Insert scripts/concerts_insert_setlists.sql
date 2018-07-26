e428976347a20143f4dfb5d112741c2348281c2a
mastersamwise <nikb248@gmail.com>
2018-07-25 22:43:16 -0400

    more

--- DataWarehouse/Database Data/Database files/Insert scripts/concerts_insert_setlists.sql
+++ DataWarehouse/Database Data/Database files/Insert scripts/concerts_insert_setlists.sql
@@ -17,7 +17,11 @@ INSERT INTO @tempTable
 VALUES
 -- https://www.setlist.fm/setlist/green-day/2009/wachovia-spectrum-philadelphia-pa-43d64fdf.html
 ( 20090721, 'Green Day', 'Song of the Century', 'Song played from a tape' ),
-( 20090721, 'Green Day', '21st Century Breakdown', '' )
+( 20090721, 'Green Day', '21st Century Breakdown', '' ),
+
+-- https://www.setlist.fm/setlist/green-day/2010/susquehanna-bank-center-camden-nj-3d5bd2b.html
+( 20100803, 'Green Day', 'Song of the Century', 'Song played from a tape' ),
+( 20100803, 'Green Day', '21st Century Breakdown', '' )
 
 INSERT INTO [concerts].[Songs]
 ( [song_title] )
