65506a94e9750c2c257362b625a0884572847642
nik <nikb248@gmail.com>
2019-03-10 21:49:44 -0400

    - more fiddling around with trying to set Drafts folder

--- QuoteEmail/EmailDriver.cs
+++ QuoteEmail/EmailDriver.cs
@@ -12,13 +12,18 @@ namespace QuoteEmail
         public static QuoteMail email;
         public static int NUMBER_OF_DRAFTS = 7;
         public static string filename = "../../../emails.txt";
+        public static string host = "imap.gmail.com";
+        public static string username = "nikb248@gmail.com";
+        public static string password = "mor4dor8";
+        public static int port = 587;
 
         public static void Main(string[] args)
         {
-            email = new QuoteMail(NUMBER_OF_DRAFTS, filename);
-            email.ReadRecipientsList();
+            email = new QuoteMail();
+            email.connect(host, port, username, password);
+            email.ReadRecipientsList(filename);
 
-            email.GenerateDraft();
+            email.GenerateDrafts(NUMBER_OF_DRAFTS);
             Console.WriteLine("Hello World!");
         }
     }
