a02659f563fc9cb1939f317445e0b2f12c04b913
nik <nikb248@gmail.com>
2019-09-30 23:38:55 +0300

    adding setup, started on scaffolding for Concerts

---
+++ GeneralServices/GeneralServices/Controllers/ConcertController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeneralServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConcertController : ControllerBase
+    {
+        [HttpPost]
+        [Route("UpsertConcert")]
+        public ActionResult<int> UpsertConcert([FromBody]string value)
+        {
+            return 1;
+        }
+
+        [HttpGet]
+        [Route("GetConcertSummary/{inConcertID}")]
+        public ActionResult<int> GetConcertSummaryByID(int inConcertID)
+        {
+
+            return inConcertID;
+        }
+
+    }
+}
---
+++ GeneralServices/GeneralServices/DataAccessLayer/ConcertDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace GeneralServices.DataAccessLayer
+{
+    public class ConcertDAL
+    {
+
+    }
+}
