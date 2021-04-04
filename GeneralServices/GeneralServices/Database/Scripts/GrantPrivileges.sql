cca27db423d7b3ef765f4120d97dd1f842ae809a
nik <nikb248@gmail.com>
2021-04-04 14:51:10 -0400

    Addressed 'must be owner of table' error

--- GeneralServices/GeneralServices/Database/Scripts/GrantPrivileges.sql
+++ GeneralServices/GeneralServices/Database/Scripts/GrantPrivileges.sql
@@ -5,3 +5,20 @@ psql general_services;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA concerts TO nik;
 
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA concerts TO nik;
+
+-- https://stackoverflow.com/questions/46773363/granted-all-privileges-on-my-postgres-table-but-still-am-getting-a-permission
+insert into concerts.venues (venue_name, venue_city, venue_state)
+values ('test', 'testcity', 'teststate')
+
+
+ALTER TABLE concerts.venues OWNER TO pi; 
+ALTER SCHEMA concerts OWNER TO pi;
+
+-- to address "must be owner of table" error
+grant pi to nik;
+
+GRANT USAGE ON SCHEMA concerts TO pi;
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA concerts TO pi;
+GRANT SELECT, UPDATE, USAGE ON ALL SEQUENCES IN SCHEMA concerts to pi;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA concerts TO pi;
+ALTER DEFAULT PRIVILEGES IN SCHEMA concerts GRANT SELECT, UPDATE, INSERT, DELETE, TRIGGER ON TABLES TO pi;
