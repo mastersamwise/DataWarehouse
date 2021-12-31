eff6aae26dc82efe000ab10e672b79c2cf0e636d
Nik <nikb248@gmail.com>
2021-12-30 19:09:47 -0500

    Created Excel to Sql reader

---
+++ CustomTools/ExcelReader/ExcelReaderDriver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Utilities
+{
+    public class ExcelReaderDriver
+    {
+        
+        public const string EXCEL_FILE = @"C:\Users\Nik\Desktop\Confirmation_Numbers_test.xlsx";
+        public const string OUTPUT_SQL_FILE = @"output\output.txt";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Reading excel file");
+
+            ExcelToSql.ParseExcelDataToSql(EXCEL_FILE, OUTPUT_SQL_FILE);
+        }
+    }
+}
