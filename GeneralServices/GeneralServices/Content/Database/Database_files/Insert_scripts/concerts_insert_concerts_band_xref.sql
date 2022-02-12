b6f998e1ab7b66d76bf398bc7b99b8f41e5a9dba
Nik Bournelis <nikb248@gmail.com>
2022-02-11 22:29:00 -0500

    Deleted SqlServer versions of Database_files, and renamed Database_files_postgres to Database_files

--- GeneralServices/GeneralServices/Content/Database/Database_files/Insert_scripts/concerts_insert_concerts_band_xref.sql
+++ GeneralServices/GeneralServices/Content/Database/Database_files/Insert_scripts/concerts_insert_concerts_band_xref.sql
@@ -1,112 +1,124 @@
-DECLARE @concertBand_xref table
-(
-	id_			int auto_increment,
-	concert_id_	int,
-	band_name_	nvarchar (100)
-)
+DO $$
 
-INSERT INTO @concertBand_xref
-( concert_id_, band_name_ )
-VALUES
-( 1, 'Green Day' ),
-( 1, 'The Bravery' ),
-( 2, 'Green Day' ),
-( 2, 'AFI' ),
-( 3, 'Blink-182' ),
-( 3, 'My Chemical Romance' ), 
-( 4, 'Blink-182' ),
-( 4, 'My Chemical Romance' ),
-( 5, 'Rise Against' ),
-( 5, 'A Day To Remember' ),
-( 6, 'Jimmy Eat World' ),
-( 6, 'All American Rejects' ),
-( 6, 'Motion City Soundtrack' ),
-( 6, 'My Chemical Romance' ),
-( 6, 'Foo Fighters' ),
-( 7, 'Macklemore' ),
-( 8, 'Yellowcard' ),
-( 9, 'Sum 41' ),
-( 10, 'Green Day' ),
-( 11, 'Goo Goo Dolls' ),
-( 11, 'Matchbox Twenty' ),
-( 12, 'Blink-182' ),
-( 13, 'Bruce Springsteen' ),
-( 14, 'Foo Fighters' ),
-( 14, 'Arctic Monkeys' ),
-( 15, 'Cheap Trick' ),
-( 15, 'Peter Frampton' ),
-( 16, 'Paul McCartney' ),
-( 17, 'Foo Fighters' ),
-( 17, 'Royal Blood' ),
-( 18, 'Marilyn Manson' ),
-( 18, 'Smashing Pumpkins' ),
-( 19, 'Foo Fighters' ),
-( 19, 'Royal Blood' ),
-( 20, 'Smashing Pumpkins' ),
-( 21, 'Motion City Soundtrack' ),
-( 22, 'Blink-182' ),
-( 22, 'Hawthorne Heights' ),
-( 23, 'Dave Matthews Band' ),
-( 24, 'PANIC! At The Disco' ),
-( 24, 'Weezer' ),
-( 24, 'Andrew McMahon in the Wilderness' ),
-( 25, 'Paul McCartney' ),
-( 26, 'Modest Mouse' ),
-( 26, 'Brand New' ),
-( 27, 'Joan Jett and the Blackhearts' ),
-( 27, 'Cheap Trick' ),
-( 27, 'Heart' ),
-( 28, 'Blink-182' ),
-( 28, 'A Day To Remember' ),
-( 29, 'Blink-182' ),
-( 29, 'A Day To Remember' ),
-( 30, 'Green Day' ),
-( 30, 'Dog Party' ),
-( 31, 'Red Hot Chili Peppers' ),
-( 32, 'Green Day' ),
-( 32, 'Against Me!' ),
-( 33, 'Dave Matthews and Tim Reynolds' ),
-( 34, 'Gorillaz' ),
-( 35, 'Cheap Trick' ),
-( 35, 'Foreigner' ),
-( 36, 'Boston' ),
-( 36, 'Joan Jett and the Blackhearts' ),
-( 37, 'Tom Petty and the Heartbreakers' ),
-( 38, 'Linkin Park' ),
-( 38, 'Blink-182' ),
-( 39, 'Animal Collective' ),
-( 39, 'Fleet Foxes' ),
-( 40, 'Green Day' ),
-( 41, 'Arcade Fire' ),
-( 42, 'Macklemore' )
+	DECLARE 
+			id_ integer;
+			concert_id_ integer;
+			band_name_ varchar (100);
+			band_id_ integer;
 
-DECLARE @id int = (SELECT TOP 1 id_ FROM @concertBand_xref)
-DECLARE @concert_id int
-DECLARE @band_name nvarchar (100)
+	BEGIN 
+		CREATE TEMP TABLE concertBand_xref_table_
+		(
+			id_col_			serial	primary key,
+			concert_id_col_	int,
+			band_id_col_	int,
+			band_name_col_	varchar (100)
+		);
+		
+		INSERT INTO concertBand_xref_table_
+		( concert_id_col_, band_name_col_ )
+		VALUES
+		( 1, 'Green Day' ),
+		( 1, 'The Bravery' ),
+		( 2, 'Green Day' ),
+		( 2, 'AFI' ),
+		( 3, 'Blink-182' ),
+		( 3, 'My Chemical Romance' ), 
+		( 4, 'Blink-182' ),
+		( 4, 'My Chemical Romance' ),
+		( 5, 'Rise Against' ),
+		( 5, 'A Day To Remember' ),
+		( 6, 'Jimmy Eat World' ),
+		( 6, 'All American Rejects' ),
+		( 6, 'Motion City Soundtrack' ),
+		( 6, 'My Chemical Romance' ),
+		( 6, 'Foo Fighters' ),
+		( 7, 'Macklemore' ),
+		( 8, 'Yellowcard' ),
+		( 9, 'Sum 41' ),
+		( 10, 'Green Day' ),
+		( 11, 'Goo Goo Dolls' ),
+		( 11, 'Matchbox Twenty' ),
+		( 12, 'Blink-182' ),
+		( 13, 'Bruce Springsteen' ),
+		( 14, 'Foo Fighters' ),
+		( 14, 'Arctic Monkeys' ),
+		( 15, 'Cheap Trick' ),
+		( 15, 'Peter Frampton' ),
+		( 16, 'Paul McCartney' ),
+		( 17, 'Foo Fighters' ),
+		( 17, 'Royal Blood' ),
+		( 18, 'Marilyn Manson' ),
+		( 18, 'Smashing Pumpkins' ),
+		( 19, 'Foo Fighters' ),
+		( 19, 'Royal Blood' ),
+		( 20, 'Smashing Pumpkins' ),
+		( 21, 'Motion City Soundtrack' ),
+		( 22, 'Blink-182' ),
+		( 22, 'Hawthorne Heights' ),
+		( 23, 'Dave Matthews Band' ),
+		( 24, 'PANIC! At The Disco' ),
+		( 24, 'Weezer' ),
+		( 24, 'Andrew McMahon in the Wilderness' ),
+		( 25, 'Paul McCartney' ),
+		( 26, 'Modest Mouse' ),
+		( 26, 'Brand New' ),
+		( 27, 'Joan Jett and the Blackhearts' ),
+		( 27, 'Cheap Trick' ),
+		( 27, 'Heart' ),
+		( 28, 'Blink-182' ),
+		( 28, 'A Day To Remember' ),
+		( 29, 'Blink-182' ),
+		( 29, 'A Day To Remember' ),
+		( 30, 'Green Day' ),
+		( 30, 'Dog Party' ),
+		( 31, 'Red Hot Chili Peppers' ),
+		( 32, 'Green Day' ),
+		( 32, 'Against Me!' ),
+		( 33, 'Dave Matthews and Tim Reynolds' ),
+		( 34, 'Gorillaz' ),
+		( 35, 'Cheap Trick' ),
+		( 35, 'Foreigner' ),
+		( 36, 'Boston' ),
+		( 36, 'Joan Jett and the Blackhearts' ),
+		( 37, 'Tom Petty and the Heartbreakers' ),
+		( 38, 'Linkin Park' ),
+		( 38, 'Blink-182' ),
+		( 39, 'Animal Collective' ),
+		( 39, 'Fleet Foxes' ),
+		( 40, 'Green Day' ),
+		( 41, 'Arcade Fire' ),
+		( 42, 'Macklemore' );
 
-DECLARE @band_id int
+		loop
+			exit when id_ is NULL;
+			concert_id_ := (SELECT concert_id_col_ FROM concertBand_xref_table_ WHERE id_col_ = id_);
+			band_name_ := (SELECT band_name_col_ FROM concertBand_xref_table_ WHERE id_col_ = id_);
 
-WHILE @id IS NOT NULL
-BEGIN
-	SET @concert_id = (SELECT concert_id_ FROM @concertBand_xref WHERE id_ = @id)
-	SET @band_name = (SELECT band_name_ FROM @concertBand_xref WHERE id_ = @id)
+			band_id_ := (SELECT band_id FROM concerts.Bands WHERE band_name = band_name_);
 
-	SET @band_id = (SELECT band_id FROM concerts.Bands WHERE band_name = @band_name)
+ 			IF band_id_ IS NULL then
+ 				raise notice 'Band name % is invalid', band_name_;
+ 				exit;
+			END IF;
 
-	IF @band_id IS NULL 
-	BEGIN
-		PRINT 'Band name ' + @band_name + ' is invalid.'
-		RETURN
-	END
-	
-	INSERT INTO concerts.ConcertBand_xref
-	( concert_id, band_id )
-	VALUES
-	( @concert_id, @band_id )
+			raise notice 'concert_id: % | band_id: %', concert_id_, band_id_;
+			INSERT INTO concerts.ConcertBand_xref
+			SELECT 
+				concert_id_, band_id_;
 
-	SET @id = (SELECT MIN( id_ ) FROM @concertBand_xref WHERE id_ > @id)
-END
---DELETE FROM concerts.ConcertBand_xref
-
-
+			id_ := (SELECT MIN( id_col_ ) FROM concertBand_xref_table_ WHERE id_col_ > id_);
+		end loop;
+		
+		-- INSERT INTO concerts.ConcertBand_xref
+		-- ( concert_id, band_id )
+		-- SELECT 
+		-- 	concert_id_col_, band_id_col_
+		-- FROM concertBand_xref_table_;
+		
+	END; 
+$$;
+SELECT * FROM concerts.concertband_xref;
+--SELECT * FROM concertBand_xref_table_;
+--DROP TABLE concertBand_xref_table_;
 
--- GeneralServices/GeneralServices/Content/Database/Database_files_postgres/Tables/common_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE common.People
-(
-	person_id				serial 	primary key	not null,
-	person_first_name		varchar (25)		not null,
-	person_last_name		varchar (25)		null,
-	person_relationship		varchar (100)		null
-);
