3753822adcc96622b0e131384d397b687e480902
nik <nikb248@gmail.com>
2022-03-24 23:31:27 -0400

    added user tables (https://github.com/mastersamwise/DataWarehouse/issues/46)

--- GeneralServices/GeneralServices/Content/Database/Database_files/Tables/common_db.sql
+++ GeneralServices/GeneralServices/Content/Database/Database_files/Tables/common_db.sql
@@ -5,3 +5,20 @@ CREATE TABLE common.People
 	person_last_name		varchar (25)		null,
 	person_relationship		varchar (100)		null
 );
+
+CREATE TABLE common.Users
+(
+	user_id			serial	primary key not null,
+	user_name		varchar (50)		not null,
+	username		varchar (25)		not null,
+	created_date	timestamp			not null,
+	last_updated	timestamp			not null,
+	is_deleted		boolean				not null default false
+);
+
+CREATE TABLE common.Credentials
+(
+	user_id				int				not null,
+	pin					varchar (25)	not null,
+	pin_last_updated	timestamp		not null
+);
