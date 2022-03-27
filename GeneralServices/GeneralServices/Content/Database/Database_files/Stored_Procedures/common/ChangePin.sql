a7da35b1da44631b12512319a5e21e2f20278585
nik <nikb248@gmail.com>
2022-03-27 01:34:00 -0400

    ChangePin sproc

--- GeneralServices/GeneralServices/Content/Database/Database_files/Stored_Procedures/common/ChangePin.sql
+++ GeneralServices/GeneralServices/Content/Database/Database_files/Stored_Procedures/common/ChangePin.sql
@@ -4,17 +4,49 @@
  *      2022-03-26      Initial creation
  */
 
- CREATE PROCEDURE common.ChangePin
+ CREATE OR REPLACE FUCNTION common.ChangePin
  (
-     IN in_user_id  integer,
-     IN in_new_pin  varchar (25)
+    in_user_id_     integer,
+    in_new_pin_     varchar (25)
  )
- BEGIN
+ RETURNS boolean
+ AS
+ $$
+    /******************************************************************/
+    /*      Declarations                                              */
+    /******************************************************************/
     DECLARE now_ timestamp := CURRENT_TIMESTAMP;
+    DECLARE out_success_ boolean := false;
 
-    UPDATE common.Credentials
-    SET     pin = in_new_pin,
-            pin_last_updated = now_;
-    WHERE user_id = in_user_id;
+    /******************************************************************/
+    /*      Logic                                                     */
+    /******************************************************************/
+    BEGIN
+        
+        /******************************************************************/
+        /*      Error Handling                                            */
+        /******************************************************************/
+        IF NOT EXISTS (SELECT 1 FROM common.Users WHERE user_id = in_user_id_) THEN
+            RAISE EXCEPTION '[common].[ChangePin]: A record in [common].[Users] with [user_id] = % does not exist.', in_user_id_;
+        
+        ELSIF NOT EXISTS (SELECT 1 FROM common.Users WHERE user_id = in_user_id_) THEN
+            RAISE EXCEPTION '[common].[ChangePin]: A record in [common].[Credentials] with [user_id] = % does not exist.', in_user_id_;
+            
+        ELSE
+        BEGIN
+            /******************************************************************/
+            /*      Body                                                      */
+            /******************************************************************/
+            UPDATE common.Credentials
+            SET     pin = in_new_pin_,
+                    pin_last_updated = now_;
+            WHERE user_id = in_user_id;
 
- END;
+            out_success_ := true;
+
+        END;
+        END IF;
+
+        RETURN out_success_;
+    END;
+$$ LANGUAGE plpgsql;
